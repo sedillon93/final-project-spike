61eaaef9319ee03c8d3496c4bec4929c13e82aa6
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-04 14:18:33 -0800

    set up 3 hidden layers in neural network instead of 1; not sure it improves accuracy yet

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CNN-spike
-testing a convolutional neural network that might be used for synthesizing speech
+# final-project spike
+testing a neural network that might be used for predicting the next chunk of a wav audio file
--- network.js
+++ network.js
@@ -1,19 +1,32 @@
 const {Layer, Network} = require ('synaptic');
 
-const inputLayer = new Layer(6);
-const hiddenLayer = new Layer(4);
-const outputLayer = new Layer(3);
+const inputLayer = new Layer(5);
+const firstHiddenLayer = new Layer(4);
+const secondHiddenLayer = new Layer(3);
+const thirdHiddenLayer = new Layer(2);
+const outputLayer = new Layer(1);
 
-inputLayer.project(hiddenLayer, Layer.connectionType.ALL_TO_ALL);
-hiddenLayer.project(outputLayer, Layer.connectionType.ALL_TO_ALL);
+let testArray = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20, 0.21, 0.22, 0.23, 0.24, 0.25];
+
+inputLayer.project(firstHiddenLayer, Layer.connectionType.ALL_TO_ALL);
+firstHiddenLayer.project(secondHiddenLayer, Layer.connectionType.ALL_TO_ALL);
+secondHiddenLayer.project(thirdHiddenLayer, Layer.connectionType.ALL_TO_ALL);
+thirdHiddenLayer.project(outputLayer, Layer.connectionType.ALL_TO_ALL);
 
 let neuralNetwork = new Network({
   input: inputLayer,
-  hidden: [hiddenLayer],
+  hidden: [firstHiddenLayer, secondHiddenLayer, thirdHiddenLayer],
   output: outputLayer,
 });
 
-const learningRate = 0.2;
+const LEARNING_RATE = 0.2;
 
-// neuralNetwork.activate([input])
-// neuralNetwork.propagate(learningRate, [desiredOutput])
+for(let i = 0; i < 200000; i++){
+  for(let j = 0; j < testArray.length - 6; j++){
+    neuralNetwork.activate(testArray.slice(j, j + 5));
+    neuralNetwork.propagate(LEARNING_RATE, [testArray[j + 6]]);
+  }
+}
+
+neuralNetwork.activate(testArray.slice(testArray.length - 5, testArray.length));
+console.log(neuralNetwork.activate(testArray.slice(testArray.length - 5, testArray.length)));
